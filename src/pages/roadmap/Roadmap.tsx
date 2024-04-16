1098f9976bbed0a877528ab9de13a9f07eb343a0
Ingrid Amaral <guid_95@hotmail.com>
2024-04-16 22:34:55 +0100

    roadmap name

---
+++ src/pages/roadmap/Roadmap.tsx
@@ -0,0 +1,45 @@
+import { useParams } from "react-router-dom";
+import Header from "@components/header/Header";
+import { Plan, PlanName, RoadmapWrapper, Step } from "./Roadmap.styled";
+import { mockedResponse } from "../../mockResponse";
+import { useEffect, useState } from "react";
+import Loading from "../loading/Loading";
+
+const Roadmap = () => {
+  let { id } = useParams<{ id: keyof typeof mockedResponse.devices }>();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (id) {
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 500);
+    }
+  }, [id]);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (id) {
+    const roadmap = mockedResponse["devices"][id]["roadmap"];
+
+    return (
+      <>
+        <Header title="Roadmap - LipoCheck" goBackTo="/devices" />
+        <RoadmapWrapper>
+          {roadmap.map((plan) => (
+            <Plan>
+              <PlanName>{plan.name}</PlanName>
+              {plan.steps.map((step) => (
+                <Step $status={step.status}>{step.name}</Step>
+              ))}
+            </Plan>
+          ))}
+        </RoadmapWrapper>
+      </>
+    );
+  }
+};
+
+export default Roadmap;
