dd070bce64f168a69c3e2a49ac6a6b8c8e246746
Ingrid Amaral <guid_95@hotmail.com>
2024-04-21 14:02:10 +0100

    use accordion on roadmap

--- src/pages/roadmap/Roadmap.styled.ts
+++ src/pages/roadmap/Roadmap.styled.ts
@@ -3,10 +3,10 @@ import styled from "styled-components";
 export const RoadmapWrapper = styled.div`
   ${({ theme: { spaces } }) => `
         display: grid;
-        grid-template-columns: repeat(5, 1fr);
         width: 100%;
         align-items: flex-start;
         gap: ${spaces.byte};
+        overflow: scroll;
     `}
 `;
 
@@ -22,86 +22,107 @@ export const Plan = styled.div`
   `}
 `;
 
-export const PlanName = styled.div`
-  ${({ theme: { spaces, colors } }) => `
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 14px;
-    color: ${colors.white};
-    gap: ${spaces.byte};
-    padding: ${spaces.bit} ${spaces.mega};
-    width: 100%;
-    background: ${colors.green};
-    position: relative;
-    box-sizing: border-box;
-    text-align: center;
-    height: fit-content;
-    
-    &::before {
-        content: '';
-        border-left: 14px solid ${colors.secondary};
-        border-top: 14px solid transparent;
-        border-bottom: 14px solid transparent;
-        position: absolute;
-        top: 0;
-        left: 0;
-    }
+export const IconContainer = styled.div<{ $isOpen?: boolean }>`
+  ${({ theme: { colors }, $isOpen }) => `  
+    width: 20px;
+    height: 20px;
 
-    &::after {
-        content: '';
-        border-left: 14px solid ${colors.green};
-        border-top: 14px solid transparent;
-        border-bottom: 14px solid transparent;
-        position: absolute;
-        top: 0;
-        left: 100%;
-        z-index: 20;
-    }
-
-    @media (max-width: 1270px) {
-        height: 50px;
-        padding: ${spaces.bytet};
-        padding-left: calc(${spaces.bit} + 25px);
-
-        &::before {
-            content: '';
-            border-left: 25px solid ${colors.secondary};
-            border-top: 25px solid transparent;
-            border-bottom: 25px solid transparent;
-            position: absolute;
-            top: 0;
-            left: 0;
-        }
-
-        &::after {
-            content: '';
-            border-left: 25px solid ${colors.green};
-            border-top: 25px solid transparent;
-            border-bottom: 25px solid transparent;
-            position: absolute;
-            top: 0;
-            left: 100%;
-            z-index: 20;
-        }
+    svg {
+      width: 20px;
+      height: 20px;
+      transform: rotate(${$isOpen ? "90deg" : "0deg"});
+      transition: all 400ms ease-in-out;
+      
+      path {
+        fill: ${$isOpen ? colors.primary : "transparent"};
+        stroke: ${colors.primary};
+      }
     }
   `}
 `;
 
-export const Step = styled.div<{ $status?: string }>`
-  ${({ theme: { spaces, colors }, $status }) => `
+export const PlanName = styled.div<{ $isOpen?: boolean }>`
+  ${({ theme: { spaces, colors, borderRadius }, $isOpen }) => `
     display: flex;
-    justify-content: center;
+    justify-content: space-between;
+    align-items: center;
     font-size: 14px;
-    color: ${$status === "done" ? colors.white : colors.black};
-    gap: ${spaces.byte};
+    color: ${colors.black};
+    //gap: ${spaces.byte};
+    padding: ${spaces.byte} ${spaces.mega};
     width: 100%;
+    border: 1px solid ${colors.primary};
+    background: ${colors.lightGray};
+    border-radius: ${borderRadius};
+    position: relative;
+    box-sizing: border-box;
+    text-align: center;
+    height: fit-content;
+    font-weight: ${$isOpen ? "bold" : "normal"};
+
+    cursor: pointer;
+
+    &:hover {
+      background: ${colors.white};
+      font-weight: bold;
+
+      svg {
+        path {
+          stroke: ${colors.primary};
+          fill: ${colors.primary};
+        }
+      }
+    }
+  `}
+`;
+
+export const StepsWrapper = styled.div<{ $isOpen?: boolean }>`
+  ${({ theme: { spaces }, $isOpen }) => `  
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+    gap: ${spaces.byte};
+    max-height: ${$isOpen ? "700px" : "0px"};
+    transition: all 400ms ease-in-out;
+    //padding: ${$isOpen ? `${spaces.byte} 0` : "0px"};
+    box-sizing: border-box;
+  `}
+`;
+
+export const Step = styled.div<{ $status?: string; $isOpen?: boolean }>`
+  ${({ theme: { spaces, colors, borderRadius }, $status, $isOpen }) => `
+    display: flex;
+    justify-content: space-between;
+    font-size: 14px;
+    color: ${colors.black};
+    gap: ${spaces.byte};
     background: ${$status === "done" ? colors.green : colors.white};
     position: relative;
-    margin-right: ${spaces.bit};
-    min-height: 70px;
     text-align: center;
-    padding: ${spaces.byte};
+    padding: ${spaces.byte} ${spaces.mega};
     box-sizing: border-box;
+    margin-left: ${spaces.tera};
+    border: 1px solid ${colors.primary};
+    background: ${colors.lightGray};
+    border-radius: ${borderRadius};
+    cursor: pointer;
+    font-weight: ${$isOpen ? "bold" : "normal"};
+    
+    &:hover {
+      background: ${colors.white};
+      font-weight: bold;
+
+      svg {
+        path {
+          stroke: ${colors.primary};
+          fill: ${colors.primary};
+        }
+      }
+    }
+
+    &:last-of-type {
+      margin-bottom: ${spaces.mega};
+    }
+
   `}
 `;
--- src/pages/roadmap/Roadmap.tsx
+++ src/pages/roadmap/Roadmap.tsx
@@ -1,13 +1,25 @@
 import { useParams } from "react-router-dom";
 import Header from "@components/header/Header";
-import { Plan, PlanName, RoadmapWrapper, Step } from "./Roadmap.styled";
+import {
+  IconContainer,
+  Plan,
+  PlanName,
+  RoadmapWrapper,
+  Step,
+  StepsWrapper,
+} from "./Roadmap.styled";
 import { mockedResponse } from "../../mockResponse";
 import { useEffect, useState } from "react";
 import Loading from "../loading/Loading";
+import { CaretRight } from "@icons";
 
 const Roadmap = () => {
   let { id } = useParams<{ id: keyof typeof mockedResponse.devices }>();
   const [isLoading, setIsLoading] = useState(true);
+  type track = {
+    [key: number]: { [key: number]: boolean };
+  };
+  const [trackAccordion, setTrackAccordion] = useState<track | null>(null);
 
   useEffect(() => {
     if (id) {
@@ -28,14 +40,82 @@ const Roadmap = () => {
       <>
         <Header title="Roadmap - LipoCheck" goBackTo="/devices" />
         <RoadmapWrapper>
-          {roadmap.map((plan) => (
-            <Plan>
-              <PlanName>{plan.name}</PlanName>
-              {plan.steps.map((step) => (
-                <Step $status={step.status}>{step.name}</Step>
-              ))}
-            </Plan>
-          ))}
+          {roadmap.map((plan, idx) => {
+            const isFirstLayerOpen = trackAccordion?.hasOwnProperty(idx);
+            return (
+              <Plan>
+                <PlanName
+                  onClick={() => {
+                    if (trackAccordion?.hasOwnProperty(idx)) {
+                      const { [idx]: i, ...rest } = trackAccordion;
+
+                      setTrackAccordion(rest);
+                    } else {
+                      setTrackAccordion({ ...trackAccordion, [idx]: {} });
+                    }
+                    console.log(trackAccordion);
+                  }}
+                  $isOpen={isFirstLayerOpen}
+                >
+                  {`${plan.name} 0/${plan.steps.length}`}
+                  <IconContainer $isOpen={isFirstLayerOpen}>
+                    <CaretRight />
+                  </IconContainer>
+                </PlanName>
+                <StepsWrapper $isOpen={isFirstLayerOpen}>
+                  {plan.steps.map((step, stepIdx) => (
+                    <Step
+                      $status={step.status}
+                      $isOpen={
+                        trackAccordion?.hasOwnProperty(idx) &&
+                        trackAccordion[idx][stepIdx] &&
+                        trackAccordion[idx][stepIdx]
+                      }
+                      onClick={() => {
+                        if (trackAccordion?.hasOwnProperty(idx)) {
+                          if (trackAccordion[idx][stepIdx]) {
+                            const { [stepIdx]: i, ...rest } =
+                              trackAccordion[idx];
+                            console.log("trackAccordion[idx][stepIdx]", 1),
+                              trackAccordion[idx][stepIdx];
+
+                            setTrackAccordion({
+                              ...trackAccordion,
+                              [idx]: {
+                                ...rest,
+                                [stepIdx]: !trackAccordion[idx][stepIdx],
+                              },
+                            });
+                          } else {
+                            console.log("trackAccordion[idx][stepIdx]", 0);
+                            setTrackAccordion({
+                              ...trackAccordion,
+                              [idx]: {
+                                ...trackAccordion[idx],
+                                [stepIdx]: true,
+                              },
+                            });
+                          }
+                        }
+                        console.log("trackAccordion step", trackAccordion);
+                      }}
+                    >
+                      {step.name}
+                      <IconContainer
+                        $isOpen={
+                          trackAccordion?.hasOwnProperty(idx) &&
+                          trackAccordion[idx][stepIdx] &&
+                          trackAccordion[idx][stepIdx]
+                        }
+                      >
+                        <CaretRight />
+                      </IconContainer>
+                    </Step>
+                  ))}
+                </StepsWrapper>
+              </Plan>
+            );
+          })}
         </RoadmapWrapper>
       </>
     );
